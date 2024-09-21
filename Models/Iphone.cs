d15ea167dd0019e52a2d5da4cd56e106ace9132d
Lucianociciliano <lucianociciliano23@gmail.com>
2024-09-20 21:25:43 -0300

    Desafio executado com sucesso e maestriagit add .

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -1,8 +1,17 @@
 namespace DesafioPOO.Models
 {
-    // TODO: Herdar da classe "Smartphone"
-    public class Iphone
+    // Herdar da classe "Smartphone"
+    public class Iphone : Smartphone
     {
-        // TODO: Sobrescrever o método "InstalarAplicativo"
+        public Iphone(string numero, string modelo, string marca, int armazenamento, int ram, bool wifi, bool bluetooth) 
+            : base(numero, modelo, marca, armazenamento, ram, wifi, bluetooth)
+        {
+        }
+
+        // Sobrescrever o método "InstalarAplicativo"
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Instalando o aplicativo {nomeApp} no iPhone...");
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,26 @@
-﻿using DesafioPOO.Models;
+﻿using System;
+using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+namespace DesafioPOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Testando a classe Nokia
+            Console.WriteLine("Testando Nokia:");
+            var meuNokia = new Nokia("123456789", "Nokia 3310", "Nokia", 16, 0, false, false);
+            meuNokia.Ligar();
+            meuNokia.ReceberLigacao();
+            meuNokia.InstalarAplicativo("WhatsApp");
+            Console.WriteLine();
+
+            // Testando a classe Iphone
+            Console.WriteLine("Testando iPhone:");
+            var meuIphone = new Iphone("987654321", "iPhone 12", "Apple", 64, 4, true, true);
+            meuIphone.Ligar();
+            meuIphone.ReceberLigacao();
+            meuIphone.InstalarAplicativo("Instagram");
+        }
+    }
+}
